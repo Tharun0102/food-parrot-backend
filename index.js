6e1a64c9fafce6cf57e58501acf5a966f33a95a0
Tharun Chowdary <65231374+Tharun0102@users.noreply.github.com>
2022-02-03 12:56:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,8 @@ app.use(error);
 //database connection
 const dbURI = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || '0.0.0.0';
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`server running at port : ${PORT}`)))
+    .then(() => app.listen(PORT, HOST, () => console.log(`server running at port : ${PORT}`)))
     .catch((err) => console.log(err));
