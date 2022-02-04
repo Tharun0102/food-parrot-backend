1d7054b090bb94a21fe6160ccdec2e073faf09ae
Tharun0102 <rallapallitharun14@gmail.com>
2022-02-04 13:10:03 +0530

    fix large size to upload issue

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const path = require('path')
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/userRoutes');
 const restaurantRoutes = require('./routes/restaurantRoutes')
@@ -20,7 +19,8 @@ dotenv.config();
 const app = express();
 
 // Middlewares 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 app.use(cors({
     origin: '*'
 }));
